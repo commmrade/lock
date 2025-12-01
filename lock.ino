f660acee1bfdaa602a2ece436c822a42f6d59ed0
klewy <cigankov807@gmail.com>
2025-12-01 19:33:28 +0300

    Update

--- lock.ino
+++ lock.ino
@@ -12,30 +12,39 @@ static MFRC522 mfrc{SS_PIN, RST_PIN};
 constexpr byte VERIFIED_KEY[]{0x74, 0xE5, 0x86, 0x04};
 
 void setup() {
-    Serial.begin(9600);
-    SPI.begin();
-    mfrc.PCD_Init();
-    delay(10);
-    mfrc.PCD_DumpVersionToSerial();
+    Serial.begin(9600); // Start serial so i can send info to the pc to output in a serial monitor
+    SPI.begin(); // Start SPI communication
+    mfrc.PCD_Init(); // Protocol handshake
+    delay(10); // Wait a bit so everything is r eady
+    mfrc.PCD_DumpVersionToSerial(); // Dump all the stuff i dotn care about
 
+    // Pins for LEDs
     pinMode(UNLOCKED_PIN, OUTPUT);
     pinMode(LOCKED_PIN, OUTPUT);
 }
 
-void loop() {
-    if (!mfrc.PICC_IsNewCardPresent()) {
-        return;
+bool card_available() {
+    if (!mfrc.PICC_IsNewCardPresent()) { // Does a REQ to check if there is a card available
+        return false;
     }
-    if (!mfrc.PICC_ReadCardSerial()) {
-        return;
+    if (!mfrc.PICC_ReadCardSerial()) { // Reads UID into an internal buffer
+        return false;
     }
+}
 
-    int selected_pin;
-    if (!memcmp(mfrc.uid.uidByte, VERIFIED_KEY, max(sizeof(VERIFIED_KEY), mfrc.uid.size))) {
-        selected_pin = UNLOCKED_PIN;
-    } else {
-        selected_pin = LOCKED_PIN;
+bool try_unlock() {
+    return !memcmp(mfrc.uid.uidByte, VERIFIED_KEY, min(sizeof(VERIFIED_KEY), mfrc.uid.size));
+}
+
+void loop() {
+    bool available = card_available();
+    if (!available) {
+        return;
     }
+    
+    bool is_unlocked = try_unlock();
+    int selected_pin = is_unlocked ? UNLOCKED_PIN : LOCKED_PIN;
+
     digitalWrite(selected_pin, HIGH);
     delay(1000);
     digitalWrite(selected_pin, LOW);
