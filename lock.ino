1569ed3521736a8f1a9adb96a58285a05e527886
klewy <cigankov807@gmail.com>
2025-12-03 16:38:45 +0300

    UART impl low level, ports low level stuff

--- lock.ino
+++ lock.ino
@@ -1,35 +1,36 @@
 #include "MFRC522.h"
 #include <string.h>
+#include "uart.h"
 
 constexpr int SS_PIN = 10;
 constexpr int RST_PIN = 9; 
 
-constexpr int UNLOCKED_PIN = 2;
-constexpr int LOCKED_PIN = 4;
+constexpr int UNLOCKED_PIN = 2; // PORTD
+constexpr int LOCKED_PIN = 4; // PORTD
 
 static MFRC522 mfrc{SS_PIN, RST_PIN};
 
 constexpr byte VERIFIED_KEY[]{0x74, 0xE5, 0x86, 0x04};
 
+static Uart0 serial{};
+
 void setup() {
-    Serial.begin(9600); // Start serial so i can send info to the pc to output in a serial monitor
+    serial.init(9600);
+ 
     SPI.begin(); // Start SPI communication
     mfrc.PCD_Init(); // Protocol handshake
     delay(10); // Wait a bit so everything is r eady
     mfrc.PCD_DumpVersionToSerial(); // Dump all the stuff i dotn care about
 
     // Pins for LEDs
-    pinMode(UNLOCKED_PIN, OUTPUT);
-    pinMode(LOCKED_PIN, OUTPUT);
+    DDRD |= (1 << UNLOCKED_PIN) | (1 << LOCKED_PIN);
 }
 
 bool card_available() {
     if (!mfrc.PICC_IsNewCardPresent()) { // Does a REQ to check if there is a card available
-        // Serial.println("Not present");
         return false;
     }
     if (!mfrc.PICC_ReadCardSerial()) { // Reads UID into an internal buffer
-        // Serial.println("Can't read UID");
         return false;
     }
     return true;
@@ -47,10 +48,8 @@ void loop() {
     
     bool is_unlocked = try_unlock();
     int selected_pin = is_unlocked ? UNLOCKED_PIN : LOCKED_PIN;
-
-    Serial.println("fuck");
-
-    digitalWrite(selected_pin, HIGH);
+    
+    PORTD |= (1 << selected_pin);
     delay(1000);
-    digitalWrite(selected_pin, LOW);
+    PORTD &= ~(1 << selected_pin);
 }
---
+++ uart.h
@@ -0,0 +1,46 @@
+
+class Uart0 {
+public:
+    Uart0() = default;
+    Uart0(unsigned long baud_rate) {
+        init(baud_rate);
+    }
+
+    void init(unsigned long baud_rate) {
+        const int delimeter = F_CPU / 16 / baud_rate - 1; // some magic numbers
+        // Set up baud rate
+        UBRR0 |= delimeter;
+
+        // Enable transmitter TX (pin 0)
+        UCSR0B |= (1 << TXEN0) | (1 << RXEN0);
+        // 8 bit, parity none, 
+        UCSR0C |= (3 << UCSZ00);
+    }
+
+    void write(char c) {
+        // Check if UDRE0 flag is set in UCSR0A (flag is 1 when buffer is free)
+        while (!(UCSR0A & (1 << UDRE0)));
+        // Write to buffer (it will then move to shift register, then is gonna be sent to usb)
+        UDR0 = c;
+    }
+
+    void print(const String& text) {
+        for (auto c : text) {
+            write(c);
+        }
+    }
+    void println(const String& text) {
+        for (auto c : text) {
+            write(c);
+        }
+        write('\n');
+    }
+
+    bool available() const {
+        // RXC flag is set when there is data to be read
+        return (UCSR0A & (1 << RXC0));
+    }
+    char read() const {
+        return UDR0;
+    }
+};
