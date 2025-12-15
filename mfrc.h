20e45ba8610238a11b3d82ee8d1c842da05e05a2
klewy <cigankov807@gmail.com>
2025-12-15 15:03:02 +0300

    Timer stuff

--- mfrc.h
+++ mfrc.h
@@ -4,6 +4,10 @@
 enum MfrcRegisters : uint8_t {
     CommandReg = 0x01,
     VersionReg = 0x37,
+    TModeReg = 0x2A,
+    TPrescalerReg = 0x2B,
+    TReloadRegH = 0x2C,
+    TReloadRegL = 0x2D,
 };
 
 enum Commands : uint8_t {
@@ -33,8 +37,10 @@ public:
         delay(50); // Oscillator startup delay
 
         // Soft reset this mf
-        write_register(MfrcRegisters::CommandReg, Commands::SoftReset);
-        delay(50); // Wait for the device to reset fully (may wanna poll here but idc)r
+        reset();
+
+        // timer setup
+        setup_timer();
 
         // TODO: Parameters, antenna stuff and so on
     }
@@ -48,6 +54,12 @@ public:
     int software_version() const {
         auto version = read_register(VersionReg);
         return version;
+    }
+
+
+    void reset() {
+        write_register(MfrcRegisters::CommandReg, Commands::SoftReset);
+        delay(50); // Wait for the device to reset fully (may wanna poll here but idc)r
     }
 
 private:
@@ -75,10 +87,23 @@ private:
 
         spi_.start_transaction(F_CPU / 4, MSB_ORDER, MODE_0);
         spi_.transfer(static_cast<uint8_t>(reg) << 1); // Write byte 0 which is address
-        auto result = spi_.transfer(value << 1);
+        auto result = spi_.transfer(value);
         spi_.end_transaction();
     
         digitalWrite(ss_pin_, HIGH);
         return result;
+    }
+
+    void setup_timer() {
+        // * The timer can also be activated automatically to meet any dedicated protocol 
+        // requirements by setting the TModeReg register’s TAuto bit to logic 1 *
+        uint8_t tmodreg_val = 0x8F;
+        write_register(TModeReg, tmodreg_val);
+        uint8_t tprescaler_val = 0xFF;
+        write_register(TPrescalerReg, tprescaler_val);
+
+        uint8_t resetval = 0xFF;
+        write_register(TReloadRegH, resetval);
+        write_register(TReloadRegL, resetval);
     }
 };
